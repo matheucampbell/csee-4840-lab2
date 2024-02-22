442fc0e482464ee3abcc3c7be61317f42de8e74c
matheucampbell <matheu.campbell.us@outlook.com>
2024-02-22 16:23:39 -0500

    bounds fixed

--- keyhandler.c
+++ keyhandler.c
@@ -1,16 +1,16 @@
 #include "keyhandler.h"
 
 int SCREEN_ROWS = 24;
-int SCREEN_COLS = 63;
+int SCREEN_COLS = 64;
 int TYPE_ROW_MIN = 18;
 
 void update_position(int keycode, int mods, int* x, int* y){
-	if (keycode == RIGHT_ARROW && *x < SCREEN_ROWS-1)
+	if (keycode == RIGHT_ARROW && *x < SCREEN_COLS-1)
 		(*x)++;
 	else if (keycode == LEFT_ARROW && *x > 0)
 		(*x)--;
 	else if (keycode == UP_ARROW && *y > 0)
 		(*y)--;
-	else if (keycode == DOWN_ARROW && *y < SCREEN_COLS-1)
+	else if (keycode == DOWN_ARROW && *y < SCREEN_ROWS-1)
 		(*y)++;
 }
