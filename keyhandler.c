0aa5ea3687214c36f0304a0799cc4fee27b856fa
matheucampbell <matheu.campbell.us@outlook.com>
2024-02-22 16:10:32 -0500

    bounds on cursor movement

--- keyhandler.c
+++ keyhandler.c
@@ -1,12 +1,16 @@
 #include "keyhandler.h"
 
+int SCREEN_ROWS = 24;
+int SCREEN_COLS = 64;
+int TYPE_ROW_MIN = 0;
+
 void update_position(int keycode, int mods, int* x, int* y){
-	if (keycode == RIGHT_ARROW)
+	if (keycode == RIGHT_ARROW && *x < SCREEN_ROWS-1)
 		(*x)++;
-	else if (keycode == LEFT_ARROW)
+	else if (keycode == LEFT_ARROW && *x > 0)
 		(*x)--;
-	else if (keycode == UP_ARROW)
+	else if (keycode == UP_ARROW && *y > 0)
 		(*y)--;
-	else if (keycode == DOWN_ARROW)
+	else if (keycode == DOWN_ARROW && *y < SCREEN_COLS-1)
 		(*y)++;
 }
