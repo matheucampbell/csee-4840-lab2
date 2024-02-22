3e0bf6fbf238c1b203f49d30a43f1094e3269bf4
matheucampbell <matheu.campbell.us@outlook.com>
2024-02-22 15:37:26 -0500

    up/down support

--- keyhandler.c
+++ keyhandler.c
@@ -1,8 +1,12 @@
 #include "keyhandler.h"
 
-void update_position(int keycode, int mods, int* x){
+void update_position(int keycode, int mods, int* x, int* y){
 	if (keycode == RIGHT_ARROW)
 		(*x)++;
 	else if (keycode == LEFT_ARROW)
 		(*x)--;
+	else if (keycode == UP_ARROW)
+		(*y)--;
+	else if (keycode == DOWN_ARROW)
+		(*y)++;
 }
--- keyhandler.h
+++ keyhandler.h
@@ -1,9 +1,11 @@
 #ifndef _KEYHANDLER_H
 #define _KEYHANDLER_H
 
-#define CURSOR 0x35
+#define CURSOR 0x23
 #define RIGHT_ARROW 0x4f
 #define LEFT_ARROW 0x50
+#define UP_ARROW 0x52
+#define DOWN_ARROW 0x51
 
 void update_position(int, int, int*);
 
