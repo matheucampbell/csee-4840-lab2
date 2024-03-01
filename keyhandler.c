f94913cd8a8af216dbab78e6684c948059f45224
matheucampbell <matheu.campbell.us@outlook.com>
2024-02-29 20:09:23 -0500

    hmm

--- keyhandler.c
+++ keyhandler.c
@@ -25,10 +25,11 @@ void update_pressed(int* new_p, uint8_t* new, uint8_t* old){
 void update_position(int pressed, int mods, char* buf, int* x, int* y){
 	int hpos = (*y - TYPE_ROW_MIN)*SCREEN_COLS + *x;
 	if (pressed == RIGHT_ARROW && hpos <= strlen(buf) - 1){
-		if (*x < SCREEN_COLS)
+		if (*x < SCREEN_COLS){
+			printf("a");
 			(*x)++;
-		if (*x == SCREEN_COLS && *y < SCREEN_ROWS - 2){
-			printf("here");
+		} else if (*x == SCREEN_COLS && *y < SCREEN_ROWS - 2){
+			printf("b");
 			*x = 0;
 			(*y)++;
 		}
