4c65fcc444a342d18eefe60968588aaeaf51188e
matheucampbell <matheu.campbell.us@outlook.com>
2024-02-29 20:43:52 -0500

    file name changes

--- lab2.c
+++ lab2.c
@@ -4,6 +4,7 @@
  *
  * Name/UNI: Please Changeto Yourname (pcy2301)
  */
+
 #include "fbputchar.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,6 +14,7 @@
 #include <unistd.h>
 #include "usbkeyboard.h"
 #include <pthread.h>
+#include "keyhandler.h"
 
 /* Update SERVER_HOST to be the IP address of
  * the chat server you are connecting to
@@ -21,7 +23,7 @@
 #define SERVER_HOST "128.59.19.114"
 #define SERVER_PORT 42000
 
-#define BUFFER_SIZE 256 
+#define BUFFER_SIZE 256
 
 /*
  * References:
@@ -34,49 +36,61 @@
 
 int sockfd; /* Socket file descriptor */
 
+pthread_t network_thread;
+void *network_thread_f(void *);
+
 struct libusb_device_handle *keyboard;
 uint8_t endpoint_address;
 
-pthread_t network_thread;
-void *network_thread_f(void *);
-char key_trans(char *keyid);
+// Cursor initialization and positioning
+char cursor = CURSOR;
+int lastx, lasty;
+int curx = 0;
+int cury = TYPE_ROW_MIN;
 
 int main()
 {
   int err, col;
 
   struct sockaddr_in serv_addr;
-
   struct usb_keyboard_packet packet;
+  struct usb_keyboard_packet packet_l = {.modifiers = 0, .reserved = 0, .keycode = {(uint8_t) 0}};  // Last packet
+
   int transferred;
   char keystate[12];
-	char sendbuf[161] = "";
-	char input[161] = "";
-	char keyvalue[2];
-	int length, cursor = 0;
+  int new_press;  // Newly pressed non-mod keys
 
+  char* textbuf = (char*) malloc(BUFFER_SIZE * sizeof(char));
+  textbuf[0] = '\0';
+  
   if ((err = fbopen()) != 0) {
     fprintf(stderr, "Error: Could not open framebuffer: %d\n", err);
     exit(1);
   }
-	
-	fbclear(0, 23);	// Clear screen
+
+  for (int row=0; row < SCREEN_ROWS; row++){
+	for (int col=0; col<SCREEN_COLS; col++){
+		fbputchar(' ', row, col, 255, 255, 255);
+	}
+  }
+
+  for (int col=0; col<SCREEN_COLS; col++)
+    fbputchar('-', TYPE_ROW_MIN-1, col, 255, 255, 255);
+
+  fbcursor(TYPE_ROW_MIN, 0);
 
   /* Draw rows of asterisks across the top and bottom of the screen */
   for (col = 0 ; col < 64 ; col++) {
     fbputchar('*', 0, col, 255, 255, 255);
-    fbputchar('*', 23, col, 255, 255, 255);
-    fbputchar('-', 20, col, 255, 255, 255);	// Devided line *
+    fbputchar('*', SCREEN_ROWS-1, col, 255, 255, 255);
   }
 
-  //fbputs("Hello CSEE 4840 World!", 4, 10);
-
   /* Open the keyboard */
-  if ( (keyboard = openkeyboard(&endpoint_address)) == NULL ) {
+  if ((keyboard = openkeyboard(&endpoint_address)) == NULL ) {
     fprintf(stderr, "Did not find a keyboard\n");
     exit(1);
   }
-    
+
   /* Create a TCP communications socket */
   if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
     fprintf(stderr, "Error: Could not create socket\n");
@@ -99,110 +113,55 @@ int main()
   }
 
   /* Start the network thread */
-  pthread_create(&network_thread, NULL, network_thread_f, NULL);
-
+  pthread_create(&network_thread, NULL, network_thread_f, NULL);   
+  
   /* Look for and handle keypresses */
   for (;;) {
+	 // Save previous cursor position before proceeding
+	lastx = curx;
+	lasty = cury;
+
     libusb_interrupt_transfer(keyboard, endpoint_address,
 			      (unsigned char *) &packet, sizeof(packet),
 			      &transferred, 0);
     if (transferred == sizeof(packet)) {
       sprintf(keystate, "%02x %02x %02x", packet.modifiers, packet.keycode[0],
 	      packet.keycode[1]);
-			keyvalue[0] = key_trans(keystate);
-			keyvalue[1] = '\0';
-			// My code
-			//
-			int j;
-			length = strlen(sendbuf);
-			if (length < 64 * 2 + 32) {
-      	if (keyvalue[0] == '\n') {
-					write(sockfd, sendbuf, strlen(sendbuf));
-					sendbuf[0] = '\0';
-					input[0] = '\0';
-					cursor = 0;
-					fbclear(21, 22);
-				} else if (keyvalue[0] == (char)8) {
-					strcpy(sendbuf + length + cursor - 1, sendbuf + length + cursor);
-				} else if (keyvalue[0] == (char)17) {
-					if (cursor < 0)
-						cursor++;
-				} else if (keyvalue[0] == (char)18) {
-					if (cursor > -length)
-						cursor--;
-				} else if (keyvalue[0] == (char)19) {
-					if (cursor < -63)
-						cursor += 64;
-				} else if (keyvalue[0] == (char)20) {
-					if (cursor > -length + 63)
-						cursor -= 64;
-				} else if (keyvalue[0] != (char)0) {
-					for (j = length; j >= length + cursor; j--) {
-						sendbuf[j + 1] = sendbuf[j];
-					}		
-					sendbuf[length + cursor] =  keyvalue[0];
-				}
-			} else if (keyvalue[0] == '\n') {
-					write(sockfd, sendbuf, strlen(sendbuf));
-					sendbuf[0] = '\0';
-					input[0] = '\0';
-					cursor = 0;
-					fbclear(21, 22);
-			} else if (keyvalue[0] == (char)8) {				// The following part is necessary when sendbuf reaches to the end
-					strcpy(sendbuf + length + cursor - 1, sendbuf + length + cursor);
-			} else if (keyvalue[0] == (char)17) {
-					if (cursor < 0)
-						cursor++;
-			} else if (keyvalue[0] == (char)18) {
-					if (cursor > -length)
-						cursor--;
-			} else if (keyvalue[0] == (char)19) {
-					if (cursor < -63)
-						cursor += 64;
-			} else if (keyvalue[0] == (char)20) {
-					if (cursor > -length + 63)
-						cursor -= 64;
-			} 
-
-			length = strlen(sendbuf);
-			int num = cursor + length;	
-			int rw = 21 + num / 64;	
-			int cl = num % 64;
-			int up = 0;
-			if (length >= 128) {
-				if (num <= 64) {
-					up = 1;
-				} else if (num < 128) {
-					if (!up)
-						rw--;
-				} else {
-					up = 0;
-					rw--;
-				}
-				if (up) {
-					strncpy(input, sendbuf, 128);
-					input[128] = '\0';
-					fbtype(21, 22, input);
-				}	else {
-					strcpy(input, sendbuf + 64);
-					fbtype(21, 22, input);	
-				}
-			} else {
-				strncpy(input, sendbuf, 128);
-				input[128] = '\0';
-				fbtype(21, 22, input);
-			}
-			
-			fbcursor(rw, cl);
-
-			printf("%s\n", sendbuf);
-
+      printf("%s\n", keystate);
+      // fbputs(keystate, 6, 0);
       if (packet.keycode[0] == 0x29) { /* ESC pressed? */
-	break;
+	 	  free(textbuf);
+		  break;
       }
+      
+      // Extract new presses from last packet and this packet
+      update_pressed(&new_press, packet.keycode, packet_l.keycode);
+	   // Change cursor position if arrows clicked
+	   update_position(new_press, packet.modifiers, textbuf, &curx, &cury);
+	   // Parse letters if letters pressed
+	   parse_symbols(new_press, packet.modifiers, textbuf, &curx, &cury);
+
+      if (curx != lastx || cury != lasty){
+		  fbputchar(' ', lasty, lastx, 255, 255, 255);
+	   } 
+		
+		// Checks for enter
+		if (new_press == ENTER){
+		  fbtype(21, 22, textbuf);
+		  write(sockfd, textbuf, strlen(textbuf));
+		  curx = 0;
+		  cury = TYPE_ROW_MIN;
+		  textbuf[0] = '\0';
+		}
+      
+		fbclear(21, 22);
+	   fbputlongs(textbuf, TYPE_ROW_MIN, 0, 2, SCREEN_COLS); 
+	  	fbcursor(cury, curx);
+
+      packet_l = packet;	
     }
   }
-
+  
   /* Terminate the network thread */
   pthread_cancel(network_thread);
 
@@ -225,57 +184,3 @@ void *network_thread_f(void *ignored)
 	return NULL;
 }
 
-char key_trans(char *keyid)
-{
-	char symbol;
-	int num[3]; 
-	int i = 0;
-	static int origin = 0, temp = 0;
-
-	char *token = strtok(keyid, " ");
-	while (token != NULL) {
-		num[i] = (int)strtol(token, NULL, 16);
-		token = strtok(NULL, " ");
-		i++;
-	}
-	if (temp != num[1]) {
-		temp = num[1];
-		if (temp == origin)
-			temp = 0;
-		else
-			origin = temp;
-	} else
-		temp = num[2];
-	printf("KEYS:%d, %d, %d\n", num[0], num[1], num[2]);
-	if (temp >= 4  && temp <= 29) {
-		if (num[0] == 2)
-			symbol = (char)(temp + 61);
-		else
-			symbol = (char)(temp + 93);
-	} else if (temp >= 30  && temp <= 38) {
-		symbol = (char)(temp + 19);
-	} else if (temp == 39) {
-		symbol = '0';
-	}	else if (temp == 44) {
-		symbol = ' ';
-	} else if (temp == 40) {
-		symbol = '\n';
-	} else if (temp == 42) {
-		symbol = 8;
-	} else if (temp >= 79 && temp <= 82) {
-		symbol = (char)(temp - 62);
-	} else if (temp == 0) {
-		symbol = 0;
-	} else if (temp == 54){
-		symbol = ',';
-	}	else if (temp == 55) {
-		symbol = '.';
-	} else if (temp == 52) {
-		if (num[0] == 2)
-			symbol = '\"';
-		else
-			symbol = '\'';
-	}
-	
-	return symbol;
-}
